ab604834cbd8bede6a31379cc098137edd8ae795
pavuko <99765321+pavuko@users.noreply.github.com>
2025-04-21 21:03:48 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,12 +4,13 @@
 #define INCLUDE_TPQUEUE_H_
 template<typename T>
 class TPQueue {
-private:
+ private:
   std::list<T> arr;
-public:
-  void push(const T& el) { 
+
+ public:
+  void push(const T& el) {
     auto pos = arr.begin();
-    while (pos!=arr.end() && pos->prior>=el.prior) {
+    while (pos != arr.end() && pos->prior>=el.prior) {
       ++pos;
     }
     arr.insert(pos, el);
@@ -17,7 +18,7 @@ public:
   T pop() {
     T temp = arr.front();
     arr.pop_front();
-    return temp; 
+    return temp;
   }
 };
 
